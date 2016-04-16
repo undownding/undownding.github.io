2f040fe04ccaa1ccb99d4439ff9a2df0ae08afd3
undownding <undownding@gmail.com>
2016-04-17 00:24:31 +0800

    remove jquery in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
 gulp.task('default', function() {
     
+});
+
+gulp.task('depends', function () {
+    return gulp.src('bower_components/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('dist/assets/js/'))
 });
